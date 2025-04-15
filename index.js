856717aff96b9f60277cb476e5e1eb9ee31bf2c0
Joao Paulo <albuquerque.paulo@academico.ifpb.edu.br>
2025-04-15 16:44:48 -0300

    adicionando api

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 import express from 'express';
-const app = express()
-const port = 3000
+const app = express();
+app.use(express.json());
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Olá Mundo!')
-})
+import Task from './model/task.js';
+
+app.get('/tasks', async (req, res)=>{
+  const tasks = await Task.findAll();
+  res.status(200).json(tasks);
+});
+
+app.post('/tasks', async (req,res)=>{
+  const newTask = await Task.create(req.body);
+  res.status(201).json(newTask);
+});
 
 app.listen(port, () => {
   console.log(`App de exemplo esta rodando na porta ${port}`)
-})
+});
